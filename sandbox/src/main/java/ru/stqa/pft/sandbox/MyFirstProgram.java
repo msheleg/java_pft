6b614d4521e66162c2c0e20538f9b56bf48daf07
msheleg <mksheleg@gmail.com>
2021-10-20 17:30:19 -0400

    added several methods/functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,26 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        hello("Masha");
+        hello("user");
+        hello("Philipp");
+        hello("Klim");
+        double len =5;
+        System.out.println("Square area with side " + len + " = " + area(len));
+
+        double a= 4;
+        double b=6;
+        System.out.println("Square rectangle area with side " + a + "and" + b+ " = " + area(a,b));
     }
+    public static void hello(String somebody) {
+        System.out.println("Hello," + somebody+ "!");
+    }
+    public static double area (double l){
+        return l*l;
+    }
+
+    public static double area(double a, double b) {
+        return a*b;
+    }
+
 }
